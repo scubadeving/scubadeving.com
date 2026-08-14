2c74441cd2f8db9231b277cc259e5b9b280072e6
Brian Valdes <bwvaldes@gmail.com>
2026-08-13 21:57:06 -0700

    refactor: centralize site constants, email aliases, nav updates

---
+++ src/data/site.ts
@@ -0,0 +1,7 @@
+export const discordUrl = "https://discord.gg/faFEbYZtX";
+export const githubOrgUrl = "https://github.com/scubadeving";
+
+export const helloEmail = "hello@scubadeving.com";
+export const betaEmail = "beta@scubadeving.com";
+export const supportEmail = "support@scubadeving.com";
+export const pressEmail = "press@scubadeving.com";
